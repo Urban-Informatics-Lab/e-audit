636c3121d7e32d95c7587655253b7d149206c076
Dipashreya Sur <60956254+dipasur@users.noreply.github.com>
2023-11-15 15:40:09 -0800

    put a generated index in simjob
    added padding to the feature_vector since the train_test_split needs the lengths of feature_vector and simjob_str to be equal

--- ML_classifiers.py
+++ ML_classifiers.py
@@ -14,7 +14,7 @@ import re
 from pathlib import Path  
 
 # the testing in this file tests whether the attribute was classified correctly (0,1) rather than calculating an MSE, because we're selecting between two attribute options
-
+# we can then calculate a correct classification rate for each method, and for each attribute in the mutli-tree method
 def correct_date_str(d):
   # '_mm/dd__hh:mm:ss'
   date, _, time = d.strip(' ').split(' ')
@@ -22,7 +22,6 @@ def correct_date_str(d):
   hour = '%02d' % (int(hour) - 1)
   return f' {date}  {hour}:{mins}:{secs}'
 
-# MAKE EXTRACT_datetime into 1 function 
 def extract_datetime(date_str):
   # date_str = correct_date_str(date_str)
   date = datetime.strptime(date_str, ' %m/%d/%Y  %H:%M:%S')
@@ -30,9 +29,6 @@ def extract_datetime(date_str):
 
 def extract_datetime_actual(date_str1):
   date1 = datetime.strptime(date_str1, '%Y-%m-%d %H:%M:%S %Z')
-  # %Z is timezone 
-  # converts a string to a datetime as long as it's in this format 
-  # FOR ME check if the object is a date time or not - check for the actual and simulated data 
   return date1
 
 def time_stats(group):
@@ -140,17 +136,16 @@ else:
 simjob = pd.read_csv(sim_job_file_path)
 simjob = simjob.drop(columns = ['WeatherFile','ModelFile'])
 simjob_str = simjob.astype(str)
-print("simjob_str columns")
-print(simjob_str.columns)
-building_params = simjob
-
-# convert building params index to integer 
-for i in building_params.index:
-    digits = re.findall('(\d+|\D+)',building_params['Job_ID'][i])
-    output = [ a for a in digits if a.isnumeric() ]
-    output = ''.join(output)
-    building_params.loc[i, 'Job_ID'] = int(output)
-    print(building_params['Job_ID'][i])
+#create new index for merging purposes 
+simjob_str.index = np.arange(1, len(simjob_str)+1)
+building_params = simjob_str.reset_index()
+print("Sim Job_Str:")
+print(simjob_str)
+building_params = simjob_str
+# create a new index for merging purposes 
+building_params.index = np.arange(1, len(building_params)+1)
+building_params = building_params.reset_index()
+print(building_params)
 
 #read in actual data - this should be normalized by square footage, columns = date_time, char_prem_id, kWh_norm_sf
 print("loading before data")
@@ -195,19 +190,19 @@ for name, group in list(grouped_actual_after): #create time series features
 print("actual data loaded")
 print(df_actual_after.head)
 
-### kNN classifying
+# kNN classifying
 le = preprocessing.LabelEncoder()
 label=le.fit_transform(job_id)
-## split the data - 80/20 training/testing
+# split the data - 80/20 training/testing
 X_train, X_test, y_train, y_test = train_test_split(feature_vector, label,random_state=135,test_size=0.2,shuffle=True)
 print("kNN y test")
 print(y_test)
 print("kNN x test")
 print(X_test)
-## train the model
+# train the model
 model = KNeighborsClassifier(n_neighbors=1)
 model.fit(X_train,y_train)
-## test on the subset
+# test on the subset 
 y_predicted = model.predict(X_test)
 print("kNN y predicted")
 print(y_predicted)
@@ -215,36 +210,43 @@ preds = pd.DataFrame(y_predicted.T, columns = ['Job_ID'])
 truth = pd.DataFrame(y_test.T, columns = ['Job_ID'])
 print("Preds Job-ID:")
 print(preds['Job_ID'])
+print("Preds Job-ID type:")
+print(type(preds['Job_ID']))
+
 print("building_params Job-ID:")
 print(building_params['Job_ID'])
-preds['Job_ID']=preds['Job_ID'].astype(int)
-building_params['Job_ID']=building_params['Job_ID'].astype(int)
-truth['Job_ID']=truth['Job_ID'].astype(int)
-preds = pd.merge(preds, building_params, how="left",on="Job_ID") #merge with actual building parameters to check how close the match was
-truth = pd.merge(truth, building_params, how="left",on="Job_ID")
+print("Building Params Job-ID type:")
+print(type(building_params['Job_ID']))
+
+preds['Job_ID']=preds['Job_ID'].astype(str)
+building_params['index']=building_params['index'].astype(str)
+truth['Job_ID']=truth['Job_ID'].astype(str)
+print("truth Job-ID:")
+print(truth['Job_ID'])
+print("truth Job-ID type:")
+print(type(truth['Job_ID']))
+
+#altered the merging code to be on the index 
+preds = pd.merge(preds, building_params, left_on="Job_ID", right_on="index") #merge with actual building parameters to check how close the match was
+truth = pd.merge(truth, building_params, left_on="Job_ID", right_on="index")
+
+print("preds columns:")
+print(preds.columns)
+print("truth columns:")
+print(truth.columns)
 print("saving results... (kNN)")
 
-# get output directory 
 output_files_path = input('Enter a directory path for the output files: ')
 print(output_files_path + "output file path loaded")
+
 output_test_path = "/".join([output_files_path, "kNN_test_preds.csv"])
-# create output directory if it does not exist 
 filepath = Path(output_test_path)  
 filepath.parent.mkdir(parents=True, exist_ok=True)  
-
 preds.to_csv(filepath, index=False)
 print("kNN_test_preds saved")
 test_truth = "/".join([output_files_path, "kNN_test_true.csv"])
 truth.to_csv(test_truth, index=False) 
 list_features = list(simjob_str.columns)
-kNN_class_correct = pd.DataFrame(columns=list_features)
-for feature in list_features:
-    kNN_class_correct[feature] =  np.array(preds[feature] == truth[feature], dtype=int) #check whether the feature was classified correctly
-
-kNN_class_correct_path = "/".join([output_files_path, "kNN_test_class_correct.csv"])
-kNN_class_correct.to_csv(kNN_class_correct_path, index=False) #binary classifications (1 = correct)
-
-kNN_rate_correct = "/".join([output_files_path, "kNN_test_rate.csv"])
 
 print("kNN test results saved!")
 
@@ -262,7 +264,15 @@ kNN_preds_after.to_csv(kNN_preds_after_path, index=False) #binary classification
 print("kNN validation results saved!")
 
 # multiple decision trees
-## split the data - 80/20 train/test split
+# split the data - 80/20 train/test split
+padding = len(simjob_str) - len(feature_vector) #added padding to make the lengths the same between feature vector and simjob_str 
+feature_vector = np.pad(feature_vector, ((0, padding), (0, 0)), mode='constant')
+print("Feature vector length: ")
+print(len(feature_vector))
+print(feature_vector)
+print("SimJob_Str length:")
+print(len(simjob_str))
+print(simjob_str)
 X_train, X_test, y_train, y_test = train_test_split(feature_vector, simjob_str,random_state=203,test_size=0.2,shuffle=True)
 print("trees y test")
 print(y_test)
@@ -296,14 +306,10 @@ for feature in list_features:
     print("y predicted")
     print(feature)
     print(y_predicted)
-    multi_class_correct[feature] =  np.array(y_predicted == y_test[feature], dtype=int) #binary classifications (1 = correct)
     multi_class_test_preds[feature] = y_predicted #predictions on test set
     mult_tree_preds_before[feature] = clf_feature.predict(actual_feature_before) #predictions on pre-retrofit data
     mult_tree_preds_after[feature] = clf_feature.predict(actual_feature_after) #predictions on post-retrofit data
-
-    multi_class_correct_path = f"{output_files_path}/multiple_trees_test_class_correct_{feature}.csv"
-    multi_class_correct[[feature]].to_csv(multi_class_correct_path, index=False)
-
+    
     multi_class_test_preds_path = f"{output_files_path}/multiple_trees_test_preds_{feature}.csv"
     multi_class_test_preds[[feature]].to_csv(multi_class_test_preds_path, index=False)
     
@@ -318,15 +324,10 @@ for feature in list_features:
 #save all of the results (for each feature tree) in one file
 print("saving results...(trees)")
 
-multi_class_correct_path = "/".join([output_files_path, "multiple_trees_test_class_correct.csv"])
-multi_class_correct.to_csv(multi_class_correct_path, index=False)
 multi_class_test_preds_path = "/".join([output_files_path, "multiple_trees_test_preds.csv"])
 multi_class_test_preds.to_csv(multi_class_test_preds_path, index=False)
 y_test_preds_path = "/".join([output_files_path, "multiple_trees_test_true.csv"])
 y_test.to_csv(y_test_preds_path, index=False)
-multiple_trees_rate = multi_class_correct.mean()
-multiple_trees_rate_path = "/".join([output_files_path, "multiple_trees_test_rate.csv"])
-multiple_trees_rate.to_csv(multiple_trees_rate_path, index=False)
 print("trees test results saved!")
 
 mult_tree_preds_before["char_prem_id"] = df_actual_before.char_prem_id.unique()
@@ -337,3 +338,4 @@ mult_tree_preds_after["char_prem_id"] = df_actual_after.char_prem_id.unique()
 mult_tree_preds_after_path = "/".join([output_files_path, "multiple_trees_validation_preds_after.csv"])
 mult_tree_preds_after.to_csv(path_or_buf = mult_tree_preds_after_path, index=False)
 print("trees validation results saved!")
+
